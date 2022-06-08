009b234d93becbaf9ff8fc40dabda5f3583be369
LucasMaiellaro <lucasrufinomaiellaro@gmail.com>
2022-06-08 05:20:39 -0300

    tela consulta

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { SigninComponent } from './components/signin/signin.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { ConsultarLivroComponent } from './components/consultar-livro/consultar-livro.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'landing' },
   { path: 'signin', component: SigninComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'landing', component: LandingComponent}
+  { path: 'landing', component: LandingComponent},
+  { path: 'consultar', component: ConsultarLivroComponent}
 ];
 
 @NgModule({
