8a2eca494cef45ff37470ede62e32fed128688f5
Arthur Viveiros <arthur.viveiros.n@gmail.com>
2022-06-15 03:41:47 -0300

    Adicionando EDIT e NEW categorias e editoras

--- frontend/src/app/components/categoria/add/categoriaadd/categoriaadd.component.ts
+++ frontend/src/app/components/categoria/add/categoriaadd/categoriaadd.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TaskService } from 'src/app/task.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-categoriaadd',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaaddComponent implements OnInit {
 
-  constructor() { }
+  idCategoria;
+  nome;
+
+  constructor(private route: ActivatedRoute, private taskService: TaskService, private rota: Router) { }
+
+  categoriaForm = new FormGroup({
+    nome: new FormControl(''),
+  });
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.idCategoria = params['id'];
+      this.getCategoriaById(params['id']);
+    });
+  }
+
+  getCategoriaById(categoriaId) {
+    this.taskService.generoById(categoriaId).subscribe((response: any) => {
+      this.nome = response.nome;
+
+      this.categoriaForm.setValue({
+        nome: response.nome
+      });
+    });
+  }
+
+  adicionarCategoria() {
+    this.taskService.newGenero(
+      this.categoriaForm.get('nome').value
+    ).subscribe((response: any) => {
+      console.log(response);
+      if (response.situacao == 'OK') {
+        this.rota.navigate(['/categoria']);
+      }
+    });
   }
 
 }
--- frontend/src/app/components/editora/add/editoraadd/editoraadd.component.ts
+++ frontend/src/app/components/editora/add/editoraadd/editoraadd.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TaskService } from 'src/app/task.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-editoraadd',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditoraaddComponent implements OnInit {
 
-  constructor() { }
+  idEditora;
+  nome;
+
+  constructor(private route: ActivatedRoute, private taskService: TaskService, private rota: Router) { }
+
+  editoraForm = new FormGroup({
+    nome: new FormControl(''),
+  });
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.idEditora = params['id'];
+      this.getEditoraById(params['id']);
+    });
+  }
+
+  getEditoraById(categoriaId) {
+    this.taskService.editoraById(categoriaId).subscribe((response: any) => {
+      this.nome = response.nome;
+
+      this.editoraForm.setValue({
+        nome: response.nome
+      });
+    });
+  }
+
+  adicionarEditora() {
+    this.taskService.newEditora(
+      this.editoraForm.get('nome').value
+    ).subscribe((response: any) => {
+      console.log(response);
+      if (response.situacao == 'OK') {
+        this.rota.navigate(['/editora']);
+      }
+    });
   }
 
 }
